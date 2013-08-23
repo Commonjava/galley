f3a8be02d4e685524e0c4c7cb3defdf7adbdaddd
John Casey <jdcasey@commonjava.org>
2013-08-22 22:07:24 -0500

    Start investigating jsoup for parsing directory listings from html.

--- transports/httpclient/src/main/java/org/commonjava/maven/galley/transport/htcli/internal/HttpListing.java
+++ transports/httpclient/src/main/java/org/commonjava/maven/galley/transport/htcli/internal/HttpListing.java
@@ -10,6 +10,8 @@ public class HttpListing
     implements ListingJob
 {
 
+    private TransferException error;
+
     private final HttpLocation location;
 
     private final String path;
@@ -29,13 +31,22 @@ public class HttpListing
     @Override
     public TransferException getError()
     {
-        throw new UnsupportedOperationException( "Not yet implemented!" );
+        // this would have been set during the call() method's execution if something went wrong.
+        return error;
     }
 
     @Override
     public ListingResult call()
-        throws Exception
     {
+        // return null if something goes wrong, after setting the error.
+        // What we should be doing here is trying to retrieve the html directory 
+        // listing, then parse out the filenames from that...
+        //
+        // They'll be links, so that's something to key in on.
+        //
+        // I'm wondering about this:
+        // http://jsoup.org/cookbook/extracting-data/selector-syntax
+        // the dependency is: org.jsoup:jsoup:1.7.2
         throw new UnsupportedOperationException( "Not yet implemented!" );
     }
 
