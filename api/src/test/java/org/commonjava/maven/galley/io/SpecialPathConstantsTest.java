01afd344d40eb4400433c396afb8bb1014be968c
John Casey <jdcasey@commonjava.org>
2016-05-18 19:35:22 -0500

    Revise mergability / metadata rating of special paths to address derivatives of metadata files. Implement default SpecialPathInfo instances in anticipation of preventing null return values (not ready for that yet).

---
+++ api/src/test/java/org/commonjava/maven/galley/io/SpecialPathConstantsTest.java
@@ -0,0 +1,79 @@
+package org.commonjava.maven.galley.io;
+
+import org.commonjava.maven.galley.model.SimpleLocation;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by jdcasey on 5/18/16.
+ */
+public class SpecialPathConstantsTest
+{
+
+    @Test
+    public void defaultFileMatchesFileInRootPath()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_FILE.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/file.txt" ),
+                    equalTo( true ) );
+    }
+
+    @Test
+    public void defaultFileMatchesPath()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_FILE.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/org/commonjava/commonjava/10/commonjava-10.pom" ),
+                    equalTo( true ) );
+    }
+
+    @Test
+    public void defaultFileDoesntMatchRootDir()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_FILE.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/" ),
+                    equalTo( false ) );
+    }
+
+    @Test
+    public void defaultFileDoesntMatchDirPath()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_FILE.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/org/commonjava/" ),
+                    equalTo( false ) );
+    }
+
+    @Test
+    public void defaultDirDoesntMatchFileInRootPath()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_DIR.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/file.txt" ),
+                    equalTo( false ) );
+    }
+
+    @Test
+    public void defaultDirDoesntMatchPath()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_DIR.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/org/commonjava/commonjava/10/commonjava-10.pom" ),
+                    equalTo( false ) );
+    }
+
+    @Test
+    public void defaultDirMatchesRootDir()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_DIR.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/" ),
+                    equalTo( true ) );
+    }
+
+    @Test
+    public void defaultDirMatchesDirPath()
+    {
+        assertThat( SpecialPathConstants.DEFAULT_DIR.getMatcher()
+                                                     .matches( new SimpleLocation( "http://foo.com" ), "/org/commonjava/" ),
+                    equalTo( true ) );
+    }
+
+}
