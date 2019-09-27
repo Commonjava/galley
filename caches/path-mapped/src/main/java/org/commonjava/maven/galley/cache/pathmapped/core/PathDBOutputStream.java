691e11959ecc806ac488570e91bb5718b5c95f42
Rui Han <ruhan@redhat.com>
2019-09-27 17:36:15 +0800

    Remove physical file when there is IOException (#295)
    * Remove physical file when there is IOException
    
    * Use Files.deleteIfExists

--- caches/path-mapped/src/main/java/org/commonjava/maven/galley/cache/pathmapped/core/PathDBOutputStream.java
+++ caches/path-mapped/src/main/java/org/commonjava/maven/galley/cache/pathmapped/core/PathDBOutputStream.java
@@ -3,6 +3,7 @@ package org.commonjava.maven.galley.cache.pathmapped.core;
 import org.commonjava.maven.galley.cache.pathmapped.model.PathKey;
 import org.commonjava.maven.galley.cache.pathmapped.model.PathMap;
 import org.commonjava.maven.galley.cache.pathmapped.spi.PathDB;
+import org.commonjava.maven.galley.cache.pathmapped.spi.PhysicalStore;
 
 import java.io.FilterOutputStream;
 import java.io.IOException;
@@ -16,9 +17,13 @@ public class PathDBOutputStream
 {
     private final PathDB pathDB;
 
+    private final PhysicalStore physicalStore;
+
     private final String fileSystem;
 
     private final String path;
+
+    private final FileInfo fileInfo;
 
     private final String fileId;
 
@@ -26,12 +31,14 @@ public class PathDBOutputStream
 
     private int size;
 
-    public PathDBOutputStream( PathDB pathDB, String fileSystem, String path, FileInfo fileInfo, OutputStream out )
+    public PathDBOutputStream( PathDB pathDB, PhysicalStore physicalStore, String fileSystem, String path, FileInfo fileInfo, OutputStream out )
     {
         super( out );
         this.pathDB = pathDB;
+        this.physicalStore = physicalStore;
         this.fileSystem = fileSystem;
         this.path = path;
+        this.fileInfo = fileInfo;
         this.fileId = fileInfo.getFileId();
         this.fileStorage = fileInfo.getFileStorage();
     }
@@ -39,21 +46,29 @@ public class PathDBOutputStream
     @Override
     public void write( int b ) throws IOException
     {
-        super.write( b );
-        size += 1;
+        try
+        {
+            super.write( b );
+            size += 1;
+        }
+        catch ( IOException e )
+        {
+            // the generated physical file should be deleted immediately
+            physicalStore.delete( fileInfo );
+            throw e;
+        }
     }
 
     @Override
     public void close() throws IOException
     {
+        super.close();
         PathMap pathMap = new PathMap();
-
         pathMap.setPathKey( getPathKey( fileSystem, path ) );
         pathMap.setCreation( new Date() );
         pathMap.setFileId( fileId );
         pathMap.setFileStorage( fileStorage );
         pathMap.setSize( size );
-
         pathDB.insert( pathMap );
     }
 }
